b5881904aa74d68b252d6fca18f2bd12dd066a72
Ikl0 <klimovs2008van@gmail.com>
2022-06-06 00:04:25 +0300

    Initial commit

---
+++ db/migrate/20220605183438_create_deals.rb
@@ -0,0 +1,11 @@
+class CreateDeals < ActiveRecord::Migration[6.1]
+  def change
+    create_table :deals do |t|
+      t.string :title
+      t.string :description
+      t.string :status
+
+      t.timestamps
+    end
+  end
+end
