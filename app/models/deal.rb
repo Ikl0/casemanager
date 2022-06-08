13606eb68b21c78fde31a279e9c41706f3da68b4
Ikl0 <klimovs2008van@gmail.com>
2022-06-08 19:56:18 +0300

    add filter by status

--- app/models/deal.rb
+++ app/models/deal.rb
@@ -1,5 +1,5 @@
 class Deal < ApplicationRecord
   belongs_to :user
-  validates :status , inclusion: { in: %w(New Active Done), message: "%{value} is not a valid status" }
+  validates :status , inclusion: { in: ['New', 'In Progress', 'Done'], message: "%{value} is not a valid status" }
   #scope :basic_info,->{where(user_id: current_user.id)}
 end
